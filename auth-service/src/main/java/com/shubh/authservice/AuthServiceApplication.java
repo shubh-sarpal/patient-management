eeae75c6284ae750e241ac3a458d73feb20939dc
Shubh <shubhsarpalofficial@gmail.com>
2025-05-01 11:38:10 +0530

    API Gateway + Initial Auth setup

---
+++ auth-service/src/main/java/com/shubh/authservice/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shubh.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
